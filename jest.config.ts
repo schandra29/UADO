34fb721f018bfb2eb20861e5902f09392e8f8600
schandra29 <sharath.chandra29@gmail.com>
2025-07-02 08:55:22 +0530

    Setup Jest with TypeScript

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  transform: {},
+  moduleNameMapper: {},
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
